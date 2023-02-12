c283548b4ba4247fb57072d730e328e5dcc3ba82
Olga Alekseeva <olgaalekseeva966@gmail.com>
2023-02-12 20:57:59 +0300

    fixed: add settings for slick-slider

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
     $('.carousel').slick({
-
+        speed: 1200,
+        // autoplay: true,
+        // autoplaySpeed: 4000,
+        fade: true,
+        cssEase: 'linear',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/prev.svg" alt="previous"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/next.svg" alt="next"></button>'
 });
 });
