9a55ab2c2b38acf77877be0dd7506aaf984eb580
Olga Alekseeva <olgaalekseeva966@gmail.com>
2023-02-17 22:13:14 +0300

    add: script for toggling catalog-items

--- js/index.js
+++ js/index.js
@@ -11,4 +11,12 @@ $(document).ready(function(){
             .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
             .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
+
+    $('.catalog-item__link').each(function(i){
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__link').eq(i).toggleClass('catalog-item__link_active');
+        })
+    })
 });
